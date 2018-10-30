5811d850b898bf7d23903e350e0206ed35a96eba
Wenchao-Ma <wenchao.ma@ua.edu>
2018-10-30 11:16:12 -0500

    v 2.2.0 update - 2

--- R/zzz.R
+++ R/zzz.R
@@ -8,6 +8,6 @@ StartWelcomeMessage <- function(){
   paste("GDINA Package",
         " [Version ", utils::packageDescription("GDINA")$Version,
         "; ",utils::packageDescription("GDINA")$Date, "]\n",
-        "For examples and more information, \nsee https://wenchao-ma.github.io/GDINA\n",
+        "More information: https://wenchao-ma.github.io/GDINA\n",
         sep="")
 }
